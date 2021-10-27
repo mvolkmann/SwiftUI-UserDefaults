d5a23d61a52f315ebec9880b3d573f5cd3b4a1e6
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-27 13:16:22 -0500

    initial version

--- SwiftUI-UserDefaults/ContentView.swift
+++ SwiftUI-UserDefaults/ContentView.swift
@@ -1,16 +1,74 @@
-//
-//  ContentView.swift
-//  SwiftUI-UserDefaults
-//
-//  Created by R. Mark Volkmann on 10/27/21.
-//
-
 import SwiftUI
 
+//struct Dog: Codable, CustomStringConvertible, Identifiable {
+struct Dog: Codable, Identifiable {
+    var name: String
+    var breed: String
+    //var description: String { "\(name) is a \(breed)"}
+    var id: String { name }
+    
+    // Can also specify different names to use for JSON keys.
+    enum CodingKeys: CodingKey {
+        case name
+        case breed
+    }
+}
+
+func getData<T>(for key: String, defaultingTo defaultValue: T) -> T where T: Decodable {
+    if let data = UserDefaults.standard.data(forKey: key) {
+        print("init: data =", data)
+        if let decoded = try? JSONDecoder().decode(T.self, from: data) {
+            return decoded
+        }
+    }
+    return defaultValue
+}
+
+func setData<T>(for key: String, to value: T) where T: Encodable {
+    if let encoded = try? JSONEncoder().encode(value) {
+        if let json = String(data: encoded, encoding: .utf8) {
+            print("setData: json =", json)
+        }
+        UserDefaults.standard.set(encoded, forKey: key)
+    }
+}
+
 struct ContentView: View {
+    private static let KEY = "dogs"
+    
+    @State private var breed = ""
+    @State private var name = ""
+    @State private var dogs: [Dog] // can't initialize this to an empty array!
+    
+    init() {
+        dogs = getData(for: ContentView.KEY, defaultingTo: [])
+    }
+    
+    func save() {
+        setData(for: ContentView.KEY, to: dogs)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Form {
+                TextField("Name", text: $name)
+                TextField("Breed", text: $breed)
+                Button("Add") {
+                    dogs.append(Dog(name: name, breed: breed))
+                    save()
+                    name = ""
+                    breed = ""
+                }
+            }
+            Section(header: Text("Dogs")) {
+                List {
+                    ForEach(dogs) { dog in
+                        //Text(String(describing: dog))
+                        Text("\(dog.name) is a \(dog.breed)")
+                    }
+                }
+            }
+        }
     }
 }
 
