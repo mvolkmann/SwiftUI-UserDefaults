38eadedceeabf96905d321d4469f051b526370de
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-27 10:57:55 -0500

    Initial Commit

---
+++ SwiftUI-UserDefaults/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-UserDefaults
+//
+//  Created by R. Mark Volkmann on 10/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-UserDefaults/SwiftUI_UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UserDefaultsApp.swift
+//  SwiftUI-UserDefaults
+//
+//  Created by R. Mark Volkmann on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
